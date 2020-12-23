cab4093de2a60782cac33dada44c362e98ef75e8
Timt <61216890+TimothyH808@users.noreply.github.com>
2020-12-22 14:31:20 -1000

    Home Page

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Slideshow animation https://codepen.io/Coding_Journey/pen/mZdOGJ BEGINS
+const slideshowImages = document.querySelectorAll(".intro .slideshow-img");
+
+const nextImageDelay = 5000;
+let currentImageCounter = 0;
+
+slideshowImages[currentImageCounter].style.opacity = 1;
+
+setInterval(nextImage,nextImageDelay);
+
+function nextImage(){
+  // slideshowImages[currentImageCounter].style.opacity = 0;
+  slideshowImages[currentImageCounter].style.zIndex = -2;
+  const tempCounter = currentImageCounter;
+  setTimeout(() => {
+  slideshowImages[tempCounter].style.opacity = 0;
+},1000);3
+  currentImageCounter = (currentImageCounter + 1) % slideshowImages.length;
+  slideshowImages[currentImageCounter].style.opacity = 1;
+  slideshowImages[currentImageCounter].style.zIndex = -1;
+}
+
+// Slideshow animation by "coding journey" ENDS
